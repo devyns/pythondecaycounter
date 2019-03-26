6371331470554d81d8325a88be4d7813938f2edd
Devyn Sharpe <devyn@devynsharpe.co.uk>
2019-03-26 20:31:34 +0000

    Changed the while loop so it includes left is greater than 0

--- decaycounter.py
+++ decaycounter.py
@@ -1,8 +1,9 @@
 from random import *
 rolls = 100
 count = 0
+left = 1
 
-while count < 30:
+while left > 0 and count < 30 :
     count = count + 1
     list = [randint(1, 6) for p in range(rolls)]
     decay = list.count(1)
