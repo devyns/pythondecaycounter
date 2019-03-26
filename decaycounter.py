bb0c691e9a0e9bfbe7516349aff0e618141cc657
Devyn Sharpe <devyn@devynsharpe.co.uk>
2019-03-26 16:19:21 +0000

    Inital Commit

---
+++ decaycounter.py
@@ -0,0 +1,20 @@
+from random import randint
+rolls = 100
+count = -1
+
+while count < 30:
+    count = count + 1
+    number = [randint(1, 6) for p in range(1)]
+    list = [randint(1, 6) for p in range(rolls)]
+    decay = list.count(1)
+    left = rolls - decay
+    rolls = rolls - decay
+
+    print(count)
+    print(decay, "Decayed")
+    print(left, "Left")
+    print(list)
+    print()
+
+
+
